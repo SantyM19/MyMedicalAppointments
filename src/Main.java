81e241663732db6167c9a0115d59c4397b4fbab5
Santiago <santiagomejiad19@hotmail.com>
2020-09-02 20:07:44 -0500

    Firt commit, this proyect is about My Medical Apointments

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[]args){
+        // Declarar el objeto ---- Instanciar el objeto
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "El Santy";
+        myDoctor.showName();
+    }
+}
