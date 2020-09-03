a8026b10706242c139f560a4bc074e752ed508ad
Santiago <santiagomejiad19@hotmail.com>
2020-09-02 20:26:23 -0500

    Created Doctor Builder

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,11 +1,22 @@
+import javax.print.Doc;
+
 public class Doctor {
-    // Atributos:
+    //Atributos
     int id;
     String name;
     String speciality;
 
-    // Comportamientos (métodos):
-    public void showName() {
-        System.out.print(name);
+    Doctor(){
+        System.out.println("Construyendo el Objeto Doctor");
     }
+
+    Doctor(String name){
+        System.out.println("El nombre del Doctor asignado es: " + name);
+    }
+
+    //Comportamientos
+    public void showName(){
+        System.out.println(name);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[]args){
         // Declarar el objeto ---- Instanciar el objeto
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "El Santy";
+        Doctor myDoctor = new Doctor("Riguel");
+        myDoctor.name = "Rizen";
         myDoctor.showName();
     }
 }
